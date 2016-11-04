073174df315fa33107a06e6006d5bb6c206b6211
mcn42 <michael.nilsen@gmail.com>
2016-11-04 10:37:57 -0400

    new changes

--- TriggerServer/src/main/java/org/mtahq/pfc/turnstile/PiPulser.java
+++ TriggerServer/src/main/java/org/mtahq/pfc/turnstile/PiPulser.java
@@ -7,6 +7,8 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
+import javax.annotation.PreDestroy;
+
 public class PiPulser {
     private static final long ACCEPT_PULSE_WIDTH_MILLIS = 40;
     private static final long BANKED_PULSE_WIDTH_MILLIS = 50;
@@ -14,20 +16,25 @@ public class PiPulser {
     
     private static final Pin ACCEPT_PIN_ID = RaspiPin.GPIO_04;
     private static final Pin BANKED_PIN_ID = RaspiPin.GPIO_05;
+    private GpioPinDigitalOutput acceptPin = null;
+    private GpioPinDigitalOutput bankedPin = null;
+    private final GpioController gpio = GpioFactory.getInstance();
         
     public PiPulser() {
         super();
+        acceptPin = gpio.provisionDigitalOutputPin(ACCEPT_PIN_ID, "Accept", PinState.LOW);
+        bankedPin = gpio.provisionDigitalOutputPin(BANKED_PIN_ID, "Banked", PinState.LOW);
     }
     
     public void sendOneSequence() {
-        final GpioController gpio = GpioFactory.getInstance();
-        
-        final GpioPinDigitalOutput acceptPin = gpio.provisionDigitalOutputPin(ACCEPT_PIN_ID, "Accept", PinState.LOW);
-        final GpioPinDigitalOutput bankedPin = gpio.provisionDigitalOutputPin(BANKED_PIN_ID, "Banked", PinState.LOW);
         
         acceptPin.pulse(ACCEPT_PULSE_WIDTH_MILLIS, true); // set second argument to 'true' use a blocking call
         Utils.sleep(INTER_PULSE_GAP_MILLIS);
         bankedPin.pulse(BANKED_PULSE_WIDTH_MILLIS, true);
-        gpio.shutdown();
+    }
+    
+    @PreDestroy
+    public void preDestroy() {
+        if(this.gpio != null) this.gpio.shutdown();
     }
 }
--- TriggerServer/src/main/java/org/mtahq/pfc/turnstile/TriggerServer.java
+++ TriggerServer/src/main/java/org/mtahq/pfc/turnstile/TriggerServer.java
@@ -1,13 +1,25 @@
 package org.mtahq.pfc.turnstile;
 
+import java.awt.Toolkit;
+
+import java.io.File;
 import java.io.IOException;
 
 import java.io.PrintWriter;
 
 
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeSet;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.annotation.PreDestroy;
+
+import javax.print.attribute.standard.Media;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,15 +31,23 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class TriggerServer {
 
     private static final int SERVER_PORT = 8080;
+    private static final long LOCKOUT_PERIOD = 10000L;
 
     private PiPulser ppulse = new PiPulser();
     private Server server = null;
-    
+
     private UUID currentUuid = UUID.randomUUID();
+    private Set<UUID> uuidSet = new TreeSet<UUID>();
+    private AtomicBoolean locked = new AtomicBoolean(false);
+    private Timer lockTimer = new Timer();
+
+    private String supervisorUrl = null;
 
     public TriggerServer() {
         super();
@@ -36,28 +56,42 @@ public class TriggerServer {
 
     private void setup() {
         server = new Server(SERVER_PORT);
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[] { "home.html" });
+        resource_handler.setResourceBase("src/main/webapp");
 
         ContextHandler context = new ContextHandler("/");
-        context.setContextPath("/");
-        context.setHandler(new RootHandler());
+        context.setHandler(resource_handler);
 
         ContextHandler contextFR = new ContextHandler("/accept");
         contextFR.setHandler(new AcceptHandler());
-        
+
         ContextHandler context3 = new ContextHandler("/uuid");
         context3.setHandler(new UUIDHandler());
 
+        ContextHandler context4 = new ContextHandler("/poll");
+        context4.setHandler(new PollingHandler());
+
+        ContextHandler context5 = new ContextHandler("/subtrigger");
+        context5.setHandler(new SubTriggerHandler());
+
         ContextHandlerCollection contexts = new ContextHandlerCollection();
-        contexts.setHandlers(new Handler[] { context, contextFR,context3 });
+        contexts.setHandlers(new Handler[] { context, contextFR, context3, context4, context5, new DefaultHandler() });
 
         server.setHandler(contexts);
     }
 
     public void start() {
         try {
+            File f = new File(".");
+            Utils.getLogger().info(String.format("Base directory '%s'", f.getAbsolutePath()));
             Utils.getLogger().info("Server is starting...");
             server.start();
             server.join();
+
+            Utils.getLogger().info(String.format("Base directory '%s'", f.getAbsolutePath()));
+
             Utils.getLogger().info("Server has started");
         } catch (Exception e) {
             Utils.getLogger().log(Level.SEVERE, "A server startup error occurred", e);
@@ -66,9 +100,36 @@ public class TriggerServer {
 
     }
 
+    public void setSupervisorUrl(String supervisorUrl) {
+        this.supervisorUrl = supervisorUrl;
+    }
+
+    public String getSupervisorUrl() {
+        return supervisorUrl;
+    }
+
+    public boolean isSupervisor() {
+        return this.supervisorUrl == null;
+    }
+
     public static void main(String[] args) {
         TriggerServer triggerServer = new TriggerServer();
+        if (args.length > 0) {
+            triggerServer.setSupervisorUrl(args[0]);
+        }
         triggerServer.start();
+    }
+
+    private void changeUUID() {
+        synchronized (this) {
+            if (!this.uuidSet.remove(this.currentUuid)) {
+                Utils.getLogger().warning("UUID not found for removal: " + this.currentUuid);
+            }
+
+            this.currentUuid = UUID.randomUUID();
+            Utils.getLogger().info(String.format("Current UUID changed to %s", this.currentUuid));
+            this.uuidSet.add(this.currentUuid);
+        }
     }
 
     class AcceptHandler extends AbstractHandler {
@@ -76,23 +137,83 @@ public class TriggerServer {
         @Override
         public void handle(String string, Request request, HttpServletRequest httpServletRequest,
                            HttpServletResponse httpServletResponse) throws IOException, ServletException {
-            //  Accept only POSTs
-            //            if(!request.getMethod().equals("POST")) {
+            if (locked.get()) {
+                Utils.getLogger().info("Locked, Accept request ignored");
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                request.setHandled(true);
+                return;
+            }
+            //      Accept only POSTs
+            //            if (!request.getMethod().equalsIgnoreCase("POST")) {
             //                httpServletResponse.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
             //                return;
             //            }
-            
+
             //  Accept only requests containig the UUID as a paramter
-            String uuid = httpServletRequest.getParameter("UUID");
-            if(uuid == null || !uuid.equals(currentUuid.toString())) {
-                httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-            currentUuid = UUID.randomUUID();
+            //            String uuid = httpServletRequest.getParameter("UUID");
+            //            if (uuid == null || !uuid.equals(currentUuid.toString())) {
+            //                httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            //                return;
+            //            }
+            lockTimer.schedule(new LockoutTask(), 0L);
+
             Utils.getLogger().info("Accept request received");
             ppulse.sendOneSequence();
+            //Toolkit.getDefaultToolkit().beep();
+            //playSound();
+
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
             request.setHandled(true);
+        }
+    }
+    
+    private void playSound() {
+        try {
+            Runtime.getRuntime().exec("afplay //Users//mnilsen//Downloads//Doorbell-SoundBible.com-516741062.wav");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class SubTriggerHandler extends AbstractHandler {
+
+        @Override
+        public void handle(String string, Request request, HttpServletRequest httpServletRequest,
+                           HttpServletResponse httpServletResponse) throws IOException, ServletException {
+            changeUUID();
+            Utils.getLogger().info("Subordinate Trigger notification received");
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            request.setHandled(true);
+        }
+    }
+
+    class PollingHandler extends AbstractHandler {
+
+        @Override
+        public void handle(String string, Request request, HttpServletRequest httpServletRequest,
+                           HttpServletResponse httpServletResponse) throws IOException, ServletException {
+            
+            String[] uuids = httpServletRequest.getParameterValues("uuid");
+            if (uuids == null || uuids.length == 0) {
+                //  No UUID parameter, return error
+                httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+            
+            //  If locked, wait
+            if (locked.get()) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else
+            //  If UUID is in uuidSet, the token is LIVE, so  we return a "No Content" code
+            if (uuidSet.contains(uuids[0])) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else
+            //  UUID is not found, token expended, return OK
+            {
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            }
+
+            
         }
     }
 
@@ -112,7 +233,7 @@ public class TriggerServer {
             request.setHandled(true);
         }
     }
-    
+
     class UUIDHandler extends AbstractHandler {
 
         @Override
@@ -128,5 +249,23 @@ public class TriggerServer {
 
             request.setHandled(true);
         }
+    }
+
+    class LockoutTask extends TimerTask {
+
+        @Override
+        public void run() {
+            locked.set(true);
+            try {
+                Thread.sleep(LOCKOUT_PERIOD);
+            } catch (InterruptedException e) {
+            }
+            locked.lazySet(false);
+        }
+    }
+    
+    @PreDestroy
+    public void preDestroy() {
+        if(this.ppulse != null) this.ppulse.preDestroy();
     }
 }
